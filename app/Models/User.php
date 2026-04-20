d25eedc82c8c33f0aa3f7bd71e01b41e58cf5bb8
Luciano Eduardo <luciano123ab@gmail.com>
2026-04-20 18:01:16 -0300

    010(Recuperacao_Senha)

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticable
 {
+    
+    use Notifiable;
+
     public function detail() {
         //Each user has one user_details:
         return $this->hasOne(UserDetail::class);
--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -49,5 +49,10 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::loginView(function() {
             return view("auth.login");
         });
+
+        //Forgot password:
+        Fortify::requestPasswordResetLinkView(function() {
+            return view("auth.forgot-password");
+        });
     }
 }
