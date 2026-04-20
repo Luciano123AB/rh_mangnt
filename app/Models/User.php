b2a8ddd351b183e8e1aebcb3aa33611fa8577d58
Luciano Eduardo <luciano123ab@gmail.com>
2026-04-20 16:05:30 -0300

    004(VamosTestar_DadosAdmin)

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    public function detail() {
+        //Each user has one user_details:
+        return $this->hasOne(UserDetail::class);
+    }
+
+    public function department() {
+        //This user belongs to a department:
+        return $this->belongsTo(Department::class);
+    }
 }
