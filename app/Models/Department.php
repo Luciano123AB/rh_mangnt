e52433448a66ff1efd858161316d7c68076b4042
Luciano Eduardo <luciano123ab@gmail.com>
2026-04-20 16:13:44 -0300

    004(VamosTestar_DadosAdmin)

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+    public function users() {
+        //Each department can be filled with multiple users:
+        return $this->belongsToMany(User::class);
+    }
 }
--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserDetail extends Model
 {
-    //
+    public function user() {
+        //Each user has one user_details: / Each user_details belongs to a single user:
+        return $this->belongsTo(User::class);
+    }
 }
