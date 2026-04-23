df62184696fcb5e00795ca5ee7b09e00c2ed881b
Luciano Eduardo <luciano123ab@gmail.com>
2026-04-23 13:27:21 -0300

    015(Perfil_Usuario)

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -1,14 +1,14 @@
 <div class="d-flex flex-column sidebar pt-4">
     <a href="{{ route('home') }}"><i class="fas fa-home me-3"></i>Home</a>
     @can('admin')
-        <a href="#" class=""><i class="fas fa-users me-3"></i>Colaborators</a>
-        <a href="#" class=""><i class="fas fa-user-gear me-3"></i>RH Colaborators</a>
-        <a href="#" class=""><i class="fas fa-industry me-3"></i>Departaments</a>
+        <a href="#"><i class="fas fa-users me-3"></i>Colaborators</a>
+        <a href="#"><i class="fas fa-user-gear me-3"></i>RH Colaborators</a>
+        <a href="#"><i class="fas fa-industry me-3"></i>Departaments</a>
     @endcan
 
     <hr>
 
-    <a href="#" class=""><i class="fas fa-cog me-3"></i>User Profile</a>
+    <a href="{{ route('user.profile') }}"><i class="fas fa-cog me-3"></i>User Profile</a>
     
     <hr>
 
