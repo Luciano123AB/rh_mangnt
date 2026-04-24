bd7866e869c70b8204e5314b0d37388733aaa3f7
Luciano Eduardo <luciano123ab@gmail.com>
2026-04-24 20:15:11 -0300

    029(CriarColaborador_Parte1)

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,5 @@ Route::middleware("auth")->group(function() {
     //RH colaborators routes:
     Route::get("/rh-users", [RhUserController::class, "index"])->name("colaborators.rh-users");
     Route::get("/rh-users/new_colaborator", [RhUserController::class, "newColaborator"])->name("colaborators.new-colaborator");
+    Route::post("/rh-users/create_colaborator", [RhUserController::class, "createRhColaborator"])->name("colaborators.create-colaborator");
 });
