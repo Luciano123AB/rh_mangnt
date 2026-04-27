b486a728ab39201e0996446b20f32228f5451894
Luciano Eduardo <luciano123ab@gmail.com>
2026-04-27 14:54:48 -0300

    032(UserDetails_Parte2)

--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserDetail extends Model
 {
+
+    protected $fillable = [
+        "address",
+        "zip_code",
+        "city",
+        "phone",
+        "salary",
+        "admission_date"
+    ];
+
     public function user() {
         //Each user has one user_details: / Each user_details belongs to a single user:
         return $this->belongsTo(User::class);
