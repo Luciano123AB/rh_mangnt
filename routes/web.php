78d8b93e9d27b2fbcdafb9a6068b45a878ad04ec
Luciano Eduardo <luciano123ab@gmail.com>
2026-04-30 11:45:53 -0300

    040(EmailConfirmar_ContaParte2)

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RhUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(["auth"])->group(function() {
+Route::middleware("auth")->group(function() {
     Route::redirect("/", "/home");
     Route::view("/home", "home")->name("home");
 
