038c917bc4b67a6166982abea61d7f220c886f4c
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-01 17:28:55 -0300

    053(EditarColaborador_NormalParte1)

--- app/Http/Controllers/RhManagementController.php
+++ app/Http/Controllers/RhManagementController.php
@@ -87,4 +87,13 @@ class RhManagementController extends Controller
 
         return redirect()->route("rh.management.home")->with("success", "Colaborator created successfully.");
     }
+
+    public function editColaborator($id) {
+        Auth::user()->can("rh") ?: abort(403, "You are not authorized to access this page.");
+
+        $colaborator = User::with("detail")->findOrFail($id);
+        $departments = Department::where("id", ">", 2)->get();
+
+        return view("colaborators.edit-colaborator", compact("colaborator", "departments"));
+    }
 }
--- resources/views/colaborators/colaborators.blade.php
+++ resources/views/colaborators/colaborators.blade.php
@@ -47,10 +47,11 @@
                             <td>
                                 <div class="d-flex gap-3 justify-content-end">
                                     @empty ($colaborator->deleted_at)
-                                        <a href="{{ route("colaborators.details", ["id" => $colaborator->id]) }}" class="btn btn-sm btn-outline-dark ms-3"><i class="fas fa-eye me-2"></i>Details</a>
-                                        <a href="{{ route("colaborators.delete", ["id" => $colaborator->id]) }}" class="btn btn-sm btn-outline-dark ms-3"><i class="fa-regular fa-trash-can me-2"></i>Delete</a>                                        
+                                        <a href="{{ route("rh.management.edit-colaborator", ["id" => $colaborator->id]) }}" class="btn btn-sm btn-outline-dark ms-3"><i class="fa-regular fa-edit me-2"></i>Edit</a>
+                                        <a href="#" class="btn btn-sm btn-outline-dark ms-3"><i class="fa-regular fa-trash-can me-2"></i>Delete</a>
+                                        <a href="#" class="btn btn-sm btn-outline-dark ms-3"><i class="fas fa-eye me-2"></i>Details</a>                                        
                                     @else
-                                        <a href="{{ route("colaborators.restore", ["id" => $colaborator->id]) }}" class="btn btn-sm btn-outline-dark ms-3"><i class="fa-solid fa-trash-arrow-up me-2"></i>Restore</a>
+                                        <a href="#" class="btn btn-sm btn-outline-dark ms-3"><i class="fa-solid fa-trash-arrow-up me-2"></i>Restore</a>
                                     @endempty
                                 </div>
                             </td>
