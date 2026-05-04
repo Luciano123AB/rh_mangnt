b29e22753ef70059a332a238e414c2d9d68db85e
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-04 11:56:13 -0300

    063(PaginaResumo_AdministradorParte1)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,7 +12,50 @@ class AdminController extends Controller
     {
         Auth::user()->can("admin") ?: abort(403, "You are not authorized to access this page.");
 
+        //Colleact all information about the organization:
+        $data = [];
+
+        //Get total number of colaborators (deleted_at is null):
+        $data["total_colaborators"] = User::whereNull("deleted_at")->count();
+
+        //Total colaborators deleted:
+        $data["total_colaborators_deleted"] = User::onlyTrashed("deleted_at")->count();
+
+        //Total salary for all colaborators:
+        $data["total_salary"] = User::withTrashed("deleted_at")
+                                    ->with("detail")
+                                    ->get()
+                                    ->sum(function ($colaborator) {
+                                        return $colaborator->detail->salary;
+                                    });
+
+        //Total colaborators by department:
+        $data["total_colaborators_per_department"] = User::withoutTrashed()
+                                                        ->with("department")
+                                                        ->get()
+                                                        ->groupBy("department_id")
+                                                        ->map(function ($department) {
+                                                            return [
+                                                                "department" => $department->first()->department_name ?? "-",
+                                                                "total" => $department->count()
+                                                            ];
+                                                        });
+
+        //Total salary by department:
+        $data["total_salary_by_department"] = User::withoutTrashed()
+                                                    ->with("department", "detail")
+                                                    ->get()
+                                                    ->groupBy("department_id")
+                                                    ->map(function ($department) {
+                                                        return [
+                                                            "department" => $department->first()->department_name ?? "-",
+                                                            "total" => $department->sum(function ($colaborator) {
+                                                                return $colaborator->detail->salary;
+                                                            })
+                                                        ];
+                                                    });
+
         //Display admin home page.
-        return view('home');
+        return view('home', compact("data"));
     }
 }
