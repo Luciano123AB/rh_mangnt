4babfed83546f4fc7dfc2484314669fc99c5f20d
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-25 14:56:08 -0300

    079(TestarAcederHome_SemLogado)

--- TESTES.md
+++ TESTES.md
@@ -12,6 +12,6 @@
 >> - Testar a inserção de um novo colaborador com o perfil do rh.
 
 >> - Testar se um admin, após login com sucesso, consegue ver a página de colaboradores rh.
-- Testar se, sem usuário logado, é possível aceder à página home.
+>> - Testar se, sem usuário logado, é possível aceder à página home.
 - Testar se, com usuário logado, é possível aceder à página login.
 - Testar se, estando logado, é possivel aceder à página de recuperação de senha.
--- tests/Feature/AccessTest.php
+++ tests/Feature/AccessTest.php
@@ -10,3 +10,10 @@ it("tests is an admin user can see the RH users page", function () {
     //Verifica se acede com sucesso à página de RH users:
     expect($this->get("/rh-users")->status())->toBe(200);
 });
+
+it("tests if is not possible to access the home page without logged user", function () {
+    //Verificar se é possível aceder à home page:
+    expect($this->get("/home")->status())->toBe(302);
+    //Ou:
+    expect($this->get("/home")->status())->not()->toBe(200);
+});
