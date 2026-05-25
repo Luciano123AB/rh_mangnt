95bff1a75e5f909ba7c1ee9aa670eef0def24605
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-25 13:08:59 -0300

    071(TestarRota_RecuperacaoSenha)

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -15,3 +15,8 @@ it("display the login page when not logged in", function () {
     //Verifica se a página de login contém o texto "Esqueceu a sua senha?":
     expect($this->get("/login")->content())->toContain("Esqueceu a sua senha?");
 });
+
+it("display the recover password page correctly", function () {
+    expect($this->get("/forgot-password")->status())->toBe(200);
+    expect($this->get("/forgot-password")->content())->toContain("Já sei a minha senha?");
+});
