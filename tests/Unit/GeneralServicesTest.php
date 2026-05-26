a30bf68ff979150213befbe51daec723c0a0bd54
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-26 10:05:02 -0300

    083(TestesUtilitarios_Parte1)

--- tests/Unit/GeneralServicesTest.php
+++ tests/Unit/GeneralServicesTest.php
@@ -6,7 +6,6 @@ it("tests if the salary is grather than a specific amount", function () {
 
     $salary = 1000;
     $amount = 500;
-
     $result = GeneralServices::checkIfSalaryIsGreaterThan($salary, $amount);
 
     expect($result)->toBeTrue();
@@ -16,7 +15,6 @@ it("tests if the salary is not grather than a specific amount", function () {
 
     $salary = 1000;
     $amount = 1500;
-
     $result = GeneralServices::checkIfSalaryIsGreaterThan($salary, $amount);
 
     expect($result)->toBeFalse();
