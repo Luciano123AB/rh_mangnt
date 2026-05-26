771cec88c3e1f7f34b10765d7cbfec15abe3ace7
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-26 09:56:17 -0300

    082(TestesUtilitarios_CenariosProjeto)

---
+++ app/Services/GeneralServices.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+class GeneralServices
+{
+    public static function checkIfSalaryIsGreaterThan($salary, $amount) {
+        return $salary > $amount;
+    }
+
+    public static function createPhraseWithNameAndSalary($name, $salary) {
+        return "O salário do(a) $name é $salary$";
+    }
+
+    public static function getSalaryWithBonus($salary, $bonus) {
+        return $salary + $bonus;
+    }
+}
