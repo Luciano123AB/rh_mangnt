b5467191542a6c787a64479126cf48b3176b0027
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-26 10:04:25 -0300

    084(TestesUtilitarios_Parte2)

--- tests/Unit/GeneralServicesTest.php
+++ tests/Unit/GeneralServicesTest.php
@@ -6,7 +6,6 @@ it("tests if the salary is grather than a specific amount", function () {
 
     $salary = 1000;
     $amount = 500;
-
     $result = GeneralServices::checkIfSalaryIsGreaterThan($salary, $amount);
 
     expect($result)->toBeTrue();
@@ -16,8 +15,16 @@ it("tests if the salary is not grather than a specific amount", function () {
 
     $salary = 1000;
     $amount = 1500;
-
     $result = GeneralServices::checkIfSalaryIsGreaterThan($salary, $amount);
 
     expect($result)->toBeFalse();
+});
+
+it("tests if the phrase is created correctly", function () {
+
+    $name = "João Ribeiro";
+    $salary = 1000;
+    $result = GeneralServices::createPhraseWithNameAndSalary($name, $salary);
+
+    expect($result)->toBe("O salário do(a) João Ribeiro é 1000$");
 });
